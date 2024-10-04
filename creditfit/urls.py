051bb3dc4bcc291f6c1c35595ca4186020da9952
pravison <pravisonjuma09@gmail.com>
2024-10-04 10:29:07 +0300

    second commit

--- creditfit/urls.py
+++ creditfit/urls.py
@@ -19,6 +19,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.site.site_header='FINNA FINTECH'
+admin.site.site_title='FINNA FINTECH'
+
 urlpatterns = [
     path('', include('home.urls')),
     path('admin/', admin.site.urls),
